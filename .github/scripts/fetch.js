c669537c2b7dfdf71b48866839819735720dae51
PRITAM MANE <89624678+prritam@users.noreply.github.com>
2025-04-20 19:46:23 +0530

    Create fetch.js

---
+++ .github/scripts/fetch.js
@@ -0,0 +1,16 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+const coins = ['bitcoin', 'ethereum', 'dogecoin'];
+
+async function fetchPrices() {
+  const res = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${coins.join(',')}&vs_currencies=usd`);
+  const data = await res.json();
+
+  const result = `const prices = ${JSON.stringify(data, null, 2)};`;
+
+  fs.writeFileSync('prices.js', result);
+  console.log("Updated prices.js with latest data");
+}
+
+fetchPrices();
